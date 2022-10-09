dfaf1449118af9258fbf11d04c61113e22852d75
Yuuta Segawa <yuuta@192.168.100.4>
2022-10-09 20:46:52 +0300

    Updates

--- src/js/components/app.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', e => {
-  document.body.style.cssText += `--scrollTop: ${window.scrollY}px`
-});
-
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
-ScrollSmoother.create({
-  wrapper: '.site-container',
-  content: '.content'
-});
